48610b0a77382b0b03415667acda4fe38f6b35f9
chenyiti <850160757@qq.com>
2018-06-22 08:32:28 +0800

    'aa'

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -30,21 +30,29 @@ class HomeController extends Controller
     }
 
     public function getTable(){
-
+        $getword = [];
 
         //序号，词汇，注释，级别 （需要用缓存的匹配组成新数组）
+        $get = [];
+         $words = Cache::get('words');
+        for ($i=0;$i<count($words);$i++){
+            for ($j=0;$j<count($getword);$j++){
+                if ($getword[$j] == $words[$i]['word']){
+                    $get[] = $words[$i];
+                }
+            }
+        }
 
-        $words = Cache::get('words');
+        $get = array_values($get);
 
 
-        $getword = [];
         $table = "<table border=\"1\" cellspacing=\"0\" cellpadding=\"0\" width=\"90%\" align=\"center\">";
         for ($i=0;$i<count($getword);$i++){
 
             $num = $i+1;
-            $word = $getword[$i]['word'];
-            $mean = $getword[$i]['mean'];
-            $level = $getword[$i]['level'];
+            $word = $get[$i]['word'];
+            $mean = $get[$i]['mean'];
+            $level = $get[$i]['level'];
             $table .= '<tr>';
             $table .= "<td>$num</td>";
             $table .= "<td>$word</td>";
